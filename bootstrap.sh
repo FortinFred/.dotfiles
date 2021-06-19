2412a5398e30436dd0a83ac3496344155904a126
Frédéric Fortin <17817939+FortinFred@users.noreply.github.com>
2021-06-19 10:23:04 -0400

    fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,12 @@
-ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.p10kzsh ~/.p10kzsh
+ln -s ~/dotfiles/.p10k.zsh ~/.p10k.zsh
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 
 #.Oh-My-Zsh
 sudo apt update
 sudo apt install git zsh -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+chsh -s /usr/bin/zsh
 
 #powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k

df0881abea79fc7ee1c338907a16a08249df7c71
Frédéric Fortin <17817939+FortinFred@users.noreply.github.com>
2024-12-02 17:28:15 -0500

    shell: adds oh-my-zsh

--- shell/install.sh
+++ shell/install.sh
@@ -2,6 +2,31 @@
 
 setupSymlink $(pwd)/.bashrc $HOME/.bashrc
 setupSymlink $(pwd)/.zshrc $HOME/.zshrc
+setupSymlink $(pwd)/.p10k.zsh $HOME/.p10k.zsh
+
+installZsh() {
+  # if dnf command is available
+  if command -v dnf &>/dev/null; then
+    sudo dnf install -y zsh
+  elif command -v apt &>/dev/null; then
+    sudo apt install -y zsh
+  fi
+  chsh -s /usr/bin/zsh
+}
+
+installOhMyZsh() {
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended "" --keep-zshrc
+
+  rm -rf $HOME/.oh-my-zsh/custom/
+  #plugins
+  git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  #powerlevel10k
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+}
+
+installZsh
+installOhMyZsh
 
 dotfilesRC '
 # User specific environment

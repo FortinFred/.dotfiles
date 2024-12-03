54225b888c03f814ec39bacb07a28d54e9412547
Frédéric Fortin <17817939+FortinFred@users.noreply.github.com>
2024-12-02 20:56:10 -0500

    nodejs: fix global npm install

--- 20-nodejs/install.sh
+++ 20-nodejs/install.sh
@@ -4,13 +4,15 @@ NODEJS_DIR=/usr/local/lib/node/
 sudo rm -rf $NODEJS_DIR
 sudo mkdir -p $NODEJS_DIR
 wget https://nodejs.org/dist/v20.18.1/node-v20.18.1-linux-x64.tar.xz -O /tmp/nodejs.tar.xz
-sudo tar -C $NODEJS_DIR --strip-components=1 -xvf /tmp/nodejs.tar.gz
-rm -f /tmp/nodejs.tar.gz
+sudo tar -C $NODEJS_DIR --strip-components=1 -xvf /tmp/nodejs.tar.xz
+rm -f /tmp/nodejs.tar.xz
 
-sudo ln -s /usr/local/lib/node/bin/node /usr/local/bin/node
-sudo ln -s /usr/local/lib/node/bin/npm /usr/local/bin/npm
+sudo ln -s $NODEJS_DIR/bin/node /usr/local/bin/node
+sudo ln -s $NODEJS_DIR/bin/npm /usr/local/bin/npm
+
+npm config set prefix "~/.npm-global"
 
 dotfilesRC '
-# nodejs
-export PATH="$PATH:/usr/local/lib/node/bin"
+#npm 
+export PATH="$PATH:~/.npm-global/bin"
 '

da7e29b5670ebd3686619418f39dc5e7d8ccd55e
Frédéric Fortin <17817939+FortinFred@users.noreply.github.com>
2025-06-16 19:38:54 -0400

    moves treesitter to highlighting.lua

---
+++ nvim/.config/nvim/lua/features/highlighting.lua
@@ -0,0 +1,44 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    dependencies = {
+        "windwp/nvim-ts-autotag",
+    },
+    opts = {
+        autotag = {
+            enable = true,
+        },
+        ensure_installed = {
+            "json",
+            "javascript",
+            "typescript",
+            "yaml",
+            "html",
+            "css",
+            "scss",
+            "markdown",
+            "markdown_inline",
+            "bash",
+            "lua",
+            "dockerfile",
+            "gitignore",
+            "java",
+        },
+        highlight = {
+            enable = true, -- Enable syntax highlighting
+        },
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = "<C-space>",
+                node_incremental = "<C-space>",
+                scope_incremental = false,
+                node_decremental = "<bs>",
+            },
+        },
+        indent = {
+            enable = true, -- Enable indentation based on Treesitter
+        },
+    },
+}

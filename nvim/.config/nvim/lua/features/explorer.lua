989568d1cb03953720390cdceff5a9e1f78b73e7
Frédéric Fortin <17817939+FortinFred@users.noreply.github.com>
2025-06-18 14:10:22 -0400

    remap go in plus as ENTER

--- nvim/.config/nvim/lua/features/explorer.lua
+++ nvim/.config/nvim/lua/features/explorer.lua
@@ -6,6 +6,21 @@ return {
         "echasnovski/mini.files",
         version = false,
         opts = {
+            mappings = {
+                close = "q",
+                go_in = "l",
+                go_in_plus = "<ENTER>",
+                go_out = "h",
+                go_out_plus = "H",
+                mark_goto = "'",
+                mark_set = "m",
+                reset = "<BS>",
+                reveal_cwd = "@",
+                show_help = "g?",
+                synchronize = "=",
+                trim_left = "<",
+                trim_right = ">",
+            },
             windows = {
                 preview = true,
                 width_preview = 80,

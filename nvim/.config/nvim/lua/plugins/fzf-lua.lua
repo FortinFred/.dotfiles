1c8bc6c05714d03846f44d4361a64f507a3b0c7b
Frédéric Fortin <17817939+FortinFred@users.noreply.github.com>
2025-06-19 18:16:04 -0400

    fzf: ajoute un shortcut ge pour git explorer

--- nvim/.config/nvim/lua/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -10,6 +10,7 @@ return {
     -- Key mappings for file search and content search
     vim.keymap.set("n", "<leader><leader>", fzf.files, { desc = "Find Files" })
     vim.keymap.set("n", "<leader>/", fzf.live_grep, { desc = "Find in Files" })
+    vim.keymap.set("n", "<leader>ge", fzf.git_status, { desc = "Git Status Files" })
     -- Key mappings for Neovim help topics
     vim.keymap.set("n", "<leader>fh", fzf.help_tags, { desc = "Search Help Topics" })
   end,

b9b9b71f39666cfefcd3e4fb020f2f09211cf87b
Frédéric Fortin <17817939+FortinFred@users.noreply.github.com>
2026-01-14 15:34:54 -0500

    format htmlangular files on save

--- nvim/.config/nvim/lua/lsp/conform.lua
+++ nvim/.config/nvim/lua/lsp/conform.lua
@@ -12,6 +12,7 @@ require("conform").setup({
         typescript = { "prettierd", "prettier", stop_after_first = true },
         css = { "prettierd", "prettier", stop_after_first = true },
         html = { "prettierd", "prettier", stop_after_first = true },
+        htmlangular = { "prettierd", "prettier", stop_after_first = true },
         json = { "prettierd", "prettier", stop_after_first = true },
         yaml = { "prettierd", "prettier", stop_after_first = true },
         markdown = { "prettierd", "prettier", stop_after_first = true },
